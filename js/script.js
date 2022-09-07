01da3093c65f23f06b6dd5838d55d6dba31daa8f
AkinShakira <shakiraakinleye@gmail.com>
2022-09-07 22:08:56 +0100

    Products: dynamic display of products from server

--- js/script.js
+++ js/script.js
@@ -1,21 +1,86 @@
 "use strict";
-
-// function getData() {
-//   fetch(
-//     "https://jsonplaceholder.com/AkinShakira/ecommerce-website"
-//   ).then((response) => console.log(response.json()));
-//     // .then((data) => console.log(data));
-// }
+const productContainer = document.querySelector(".product__list");
 
 
-function getData() {
-  fetch("https://mockend.com/shakiraakinleye/ecommerce-website/products").then(
-    (response) => console.log(response)
-  );
-  // .then((data) => console.log(data));
+
+
+async function getProductJson() {
+  try {
+    const response = await fetch("http://localhost:3000/products");
+    const json = await response.json();
+    return json;
+    // Handle errors
+  } catch {
+    console.log(error)
+  }
 }
 
-getData();
 
 
+function renderVariants(option) {
+  const variant = `<option value="${option}">${option}</option>`
+  return variant;
+}
 
+
+function renderProducts(product) {
+  const html = `
+        <div class="product">
+          <!-- product image -->
+          <div class="product__image__container mb-2">
+            <img src= "${
+              product.imageUrl
+            }" alt="Product Image" class="product__image" >
+          </div>
+
+          <p class="product__name">${product.name}</p>
+
+          <div class="product__info">
+            <p class="product__stock__icon ${
+              product.stock > 0 ? "product--instock" : "product--soldout"
+            }"></p>
+              
+            <p class="product__stock__note mr-3">${
+              product.stock > 0 ? "In Stock" : "Sold Out"
+            }</p>
+              
+            <p class="product__price">₦${product.price}</p>
+          </div>
+
+          <div class="product__options">
+            <!-- Add a reducer, counter and adder here = - 4 + -->
+            <div class="product__quantity">
+              4
+            </div>
+            <div class="product__variant">
+              <div class="product__color">
+                <select name="product__color" id="product__color">
+                  ${product.variant.map((option) => renderVariants(option))}               
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="product__actions">
+            <button class="btn__add-favorite">
+                <img src="images/add-favorite.png">
+            </button>
+            <button class="btn__add-cart">
+                <img src="images/add-cart.png" alt="">
+            </button>
+            <button class="btn__buy">
+                Buy
+            </button>
+          </div>
+        </div>
+      `;
+    productContainer.insertAdjacentHTML("beforeend", html);
+  };
+
+
+async function displayProducts() {
+  const data = await getProductJson();
+  data.forEach((product) => renderProducts(product))
+}
+
+displayProducts();
